c6fd4831715d470970af7d68b172bef43ee84089
Luca Querci <lucabit73@gmail.com>
2018-10-31 16:49:13 +0100

    different mode of declare and use React element

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // el1 is created using jsx syntax and is equivalent to el2, both of them are object
+    const el1 = <div className='App'><h1>My elem</h1></div>;
+    const el2 = React.createElement('div', {className: 'App'}, React.createElement('h1', null, "My elem"))
+    console.log(el1);
+    console.log(el2);
+    console.log('typeof el1: ' + typeof el1);
+
     return (
-      React.createElement('div', {className: 'App'}, React.createElement('h1', null, "I'm a Baz"))
+      // can do this because of jsx syntax
+      <div className='App'><h1>Bau!</h1></div>
+      // we can return also 'el1' or 'el2'
     );
   }
 }
