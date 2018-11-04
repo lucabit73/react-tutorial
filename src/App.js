b4c89bf31ee4f959743c56602a1d4383129ac1ca
Luca Querci <lucabit73@gmail.com>
2018-11-04 12:22:57 +0100

    two way o styling elements

--- src/App.js
+++ src/App.js
@@ -30,10 +30,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Bau</h1>
-        <button onClick={() => this.switchnameHandler('Maxim')}>Click me!</button>
+        <button 
+          style={style}
+          onClick={() => this.switchnameHandler('Maxim')}>Click me!</button>
         <Person 
           name={this.state.person[0].name} 
           age={this.state.person[0].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div className="person">
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
             {props.children}
