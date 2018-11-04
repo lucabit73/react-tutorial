aa15a209d4ae5511376132997c5ce71692368679
Luca Querci <lucabit73@gmail.com>
2018-11-04 11:03:52 +0100

    simple use of state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    person: [
+      {name: "max", age: 28},
+      {name: "Ale", age: 18},
+      {name: "Paole", age: 38},
+    ]
+  }
+  switchnameHandler = () =>{
+    this.setState({
+      person: [
+        {name: "Maximilluian", age: 28},
+        {name: "Ale", age: 18},
+        {name: "Paole", age: 138},
+    ]});
+  }
   render() {
     return (
       <div className="App">
         <h1>Bau</h1>
-        <Person name="Max" age="11"/>
-        <Person name="Ale" age="3"><p>And I'm bad</p></Person>
-        <Person name="Luk" age="111"/>
+        <button onClick={this.switchnameHandler}>Click me!</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>And I'm bad</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
       </div>
     );
   }
